c05d746c2e1bad39d4bbcf7aad755a438dda2f45
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-08 21:11:35 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   array.each do |element|
     square = element ** element
-    new_array[element].push(square)
+    new_array.push(square)
   end
   new_array
 end
