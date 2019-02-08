ff4aa2df2c9669a5eeb4359e68e7c4dd96b05bfb
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-08 20:34:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   new_array = []
   array.each do |elements|
-    new_array[elements] = sqrt(array[elements])
+    new_array[elements] = array[elements] ** array[elements]
   end
 end
